e23ee2ac2ea717d22b95b440c29015842d58a01e
Guccisan <32457819+Guccisan@users.noreply.github.com>
2019-07-17 19:41:33 +0900

    株価を取得してcsvに保存

--- python/market.py
+++ python/market.py
@@ -2,6 +2,11 @@
 # 必ず２つのモジュールをインポートすること
 import requests
 from bs4 import BeautifulSoup
+#csvの読み書きするのに必要
+import pandas as pd
+
+# リストの初期化
+l = []
 
 # FANUCの株価のURL
 url = 'https://www.nikkei.com/nkd/company/?scode=6954' 
@@ -22,5 +27,17 @@ for rt in book.findAll("span", {'class':'m-stockPriceElm_value_unit'}):
     rt.extract()
 
 # タグを削除して出力
-print("Company" + ":" + "Stockprice")
+print("Company" + ":" + "Stock price")
 print(company.text + ':' + book.text)
+
+# listに会社名と株価を追加
+l.append(["Company", "Stock price"])
+l.append([company.text, book.text])
+
+# listをdataframe形式に変換(dataframeでないと書き込みできない)
+df = pd.DataFrame(l)
+
+# csvファイルに出力
+# ヘッダー（列名）、インデックス（行名）のありなしは引数header, indexにTrue or Falseで指定する。
+# encodingで文字コードを指定
+df.to_csv('output/csv_market.csv', header=False, index=False, encoding="shift_jis")
